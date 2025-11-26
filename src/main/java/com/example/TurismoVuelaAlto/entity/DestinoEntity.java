360a03ef00336ee8a5de95c50ec9878916be26c9
PieroH-del <pharryson1@alumno.uned.es>
2025-11-25 22:56:52 -0500

    cambio

--- src/main/java/com/example/TurismoVuelaAlto/entity/DestinoEntity.java
+++ src/main/java/com/example/TurismoVuelaAlto/entity/DestinoEntity.java
@@ -3,7 +3,6 @@ package com.example.TurismoVuelaAlto.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
