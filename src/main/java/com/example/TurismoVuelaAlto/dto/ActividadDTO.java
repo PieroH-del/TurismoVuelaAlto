1a101e0752c1e6022fd0224e3185f80ca933103f
PieroH-del <pharryson1@alumno.uned.es>
2025-11-25 23:02:01 -0500

    nuevo cambio

--- src/main/java/com/example/TurismoVuelaAlto/dto/ActividadDTO.java
+++ src/main/java/com/example/TurismoVuelaAlto/dto/ActividadDTO.java
@@ -5,11 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-
 
 @Getter
 @Setter
